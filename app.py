c5159dba8de956237d530793d75107ac683a35b6
Harshita Gupta <hrsht1998@gmail.com>
2021-06-18 17:34:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,6 +138,7 @@ if (rad=="desc"):
 st.header("Investors")
 st.write("See the Investors of the above mentioned companies.")
 com=st.multiselect("Select Company(s)",dcx["Company name"])
+st.write(com)
 if(len(com)>0):
   inv=df_city[df_city["Company name"].isin(com)][["Company name","Investors"]]
   inv.reset_index(drop=True, inplace=True)
