cbda556210f0ff32b48fd34e959b91f2745af826
Harshita Gupta <hrsht1998@gmail.com>
2021-06-25 20:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ st.write("See the Investors of the above mentioned companies.")
 people=st.multiselect("Select Company(s)",df_city["Company name"])
 if(len(people)>0):
   for i in range(len(people)):
-    inv=df_city[df["Company name"]==people[i]]
+    inv=df_city[df_city["Company name"]==people[i]]
 # inv=df_city[df_city["Company name"]==people]
 inv.reset_index(drop=True, inplace=True)
 st.table(inv)
