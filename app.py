92848307850321dac96bee11c74424b39bdd9f86
Harshita Gupta <hrsht1998@gmail.com>
2021-12-03 21:59:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,72 @@ def get_data():
   df["Date Joined"]=df["Date Joined"].dt.strftime("%d-%m-%Y")
   df["Valuation ($B)"] = df["Valuation ($B)"].apply(lambda x : x.replace("$","")) 
   df["Valuation ($B)"] = df["Valuation ($B)"].astype("float")
-  df.to_csv("unicornvvvv.csv")
+  df.to_csv("unicorn.csv")
+  
+def findGeocode(city):
+       
+    # try and catch is used to overcome
+    # the exception thrown by geolocator
+    # using geocodertimedout  
+    try:
+          
+        # Specify the user_agent as your
+        # app name it should not be none
+        geolocator = Nominatim(user_agent="your_app_name")
+          
+        return geolocator.geocode(city)
+      
+    except GeocoderTimedOut:
+          
+        return findGeocode(city) 
+      
+      
+def location():
+  #for cities
+  lon = {}
+  lat = {}
+  for i in tqdm(df["City"].unique()):
+      
+    if findGeocode(i) != None:
+           
+        loc = findGeocode(i)
+          
+        # coordinates returned from 
+        # function is stored into
+        # two separate list
+        lat[i]=loc.latitude
+        lon[i]=loc.longitude
+       
+    # if coordinate for a city not
+    # found, insert "NaN" indicating 
+    # missing value 
+    else:
+        lat[i]=np.nan
+        lon[i]=np.nan
+        
+  #for countries
+  longitude = {}
+  latitude = {}
+  for i in tqdm(df["Country of origin"].unique()):
+      
+    if findGeocode(i) != None:
+           
+        loc = findGeocode(i)
+          
+        # coordinates returned from 
+        # function is stored into
+        # two separate list
+        latitude[i]=loc.latitude
+        longitude[i]=loc.longitude
+       
+    # if coordinate for a city not
+    # found, insert "NaN" indicating 
+    # missing value 
+    else:
+        latitude[i]=np.nan
+        longitude[i]=np.nan
+        
+  return longitude, latitude , lat, lon
 
 st.set_page_config(layout="wide")
 
@@ -24,10 +89,15 @@ st.warning("A unicorn company, or unicorn startup, is a private company with a v
 
 #Dataset
 get_data()
-t=pd.read_csv("unicornvvvv.csv")
+dt=pd.read_csv("unicorn.csv")
 st.write(t.shape[0])
-df=pd.read_csv("700_unicorn.csv")
+# df=pd.read_csv("700_unicorn.csv")
 df["Date Joined"]=pd.DatetimeIndex(df["Date Joined"])
+longitude,latitude,lat,lon=location()
+df["longitude"]=longitude
+df["latitude"]=latitude
+df["lat"]=lat
+df["lon"]=lon
 
 
 #globe
